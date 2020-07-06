8916f471b551e1fc4507a6722ac1342df8d94822
joyce-paul <joyce_paul@hotmail.com>
2020-07-06 10:20:28 +0800

    Revised colour palette for the legend

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
 from bokeh.models.tools import HoverTool
 from bokeh.transform import factor_cmap
-from bokeh.palettes import Blues8
+from bokeh.palettes import Reds8
+# from bokeh.palettes import Blues8
 from bokeh.embed import components
-import pandas as pd
+import pandas
 
 # Read in CSV
-df = pd.read_csv('book-sales.csv')
+df = pandas.read_csv('book-sales.csv')
 
 # book = df['Book']
 # sales = df['Sales_million']
@@ -37,12 +38,13 @@ p.hbar(
     right = 'Sales_million',
     left = 0,
     height = 0.2,
-    color = "#53777a",
-    # fill_color = factor_cmap(
-    #     'Book',
-    #     palette=Blues8,
-    #     factors=book_list
-    # ),
+    # color = "#53777a",
+    fill_color = factor_cmap(
+        'Book',
+        palette=Reds8,
+        # palette=Blues8,
+        factors=book_list
+    ),
     fill_alpha = 0.6,
     source = source,
     legend = 'Book'
@@ -50,10 +52,10 @@ p.hbar(
 
 
 
-# Add legend
-p.legend.orientation='vertical'
-p.legend.location='top_right'
-p.legend.label_text_font_size='13px'
+# # Add legend
+# p.legend.orientation='vertical'
+# p.legend.location='top_right'
+# p.legend.label_text_font_size='13px'
 
 
 # Add Tooltips
@@ -73,12 +75,12 @@ p.add_tools(hover)
 
 
 # Show results
-# show(p)
+show(p)
 
-# Save file
-save(p)
+# # Save file
+# save(p)
 
-# Print div and script
-script, div = components(p)
-print(div)
-print(script)
+# # Print div and script
+# script, div = components(p)
+# print(div)
+# print(script)
